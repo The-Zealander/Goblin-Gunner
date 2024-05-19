f437ddd72867ada7917b1ea2767d22d626a67199
The-Zealander <dkzakarias@gmail.com>
2024-05-19 21:07:19 +0200

    deleted useless files

--- calc.py
+++
@@ -1,5 +0,0 @@
-import math
-
-def calculate_distance(pos1, pos2):
-    # Euclidean distance between two points (x1, y1) and (x2, y2)
-    return math.sqrt((pos1[0] - pos2[0]) ** 2 + (pos1[1] - pos2[1]) ** 2)
